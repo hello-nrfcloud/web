a8753fbe5d0073b9f59d6df38542f1da9ba7cd73
Markus Tacker <Markus.Tacker@NordicSemi.no>
2023-02-08 13:30:31 +0100

    fix(map): add note

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -7,5 +7,35 @@ const MapContainer = styled.div`
 	background-position: center;
 	background-size: cover;
 	background-repeat: no-repeat;
+	aside {
+		color: white;
+		font-style: italic;
+	}
 `
-export const Map = () => <MapContainer />
+export const Map = () => (
+	<MapContainer>
+		<aside class="container">
+			<div class="row pt-4">
+				<div class="col-6">
+					<p>
+						This is a smaller version of what then can become the new Nordic
+						World. Not like Thingy World (which is a demo that needs a human to
+						explain it), this map component should be self-explaining and guide
+						the user through what the key takeaways are here:
+					</p>
+					<ul>
+						<li>location (with and without cellular)</li>
+						<li>
+							information about the connectivity
+							<ul>
+								<li>Active network</li>
+								<li>power savings capabilities (eDRX, PSM)</li>
+								<li>SIM card</li>
+							</ul>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</aside>
+	</MapContainer>
+)
