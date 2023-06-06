4734af2685270c6bd45444b9e8d81e241c0030da
Markus Tacker <Markus.Tacker@NordicSemi.no>
2023-06-06 16:29:47 +0200

    feat: align footer design to CI

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,37 +1,74 @@
-import { SecondaryLink } from '#components/Buttons.js'
+const CopyrightYear = () => {
+	const startYear = 2023
+	const currentYear = new Date().getFullYear()
+
+	return (
+		<span>
+			{currentYear > startYear
+				? `${startYear}&ndash;${currentYear}`
+				: startYear}
+		</span>
+	)
+}
 
 export const Footer = () => (
-	<footer
-		style={{
-			backgroundColor: 'var(--color-nordic-dark-grey)',
-		}}
-		class="pt-4 pb-4"
-	>
-		<div class="container pt-4 pb-4 text-light">
+	<footer class="pt-4 pb-4 bg-light">
+		<div class="container pt-4">
 			<div class="row">
-				<div class="col-4">
-					<p>
-						<a href="https://devzone.nordicsemi.com/" target="_blank">
-							<img
-								src="/static/images/devzone-white.svg"
-								alt="{DevZone"
-								width="250"
-								height="150"
-							/>
-						</a>
-					</p>
-					<h2>Get support</h2>
-					<p>
-						In case you have any question, reach out to our community or create
-						a private support ticket.
-					</p>
-					<p>
-						<SecondaryLink
-							href="https://devzone.nordicsemi.com/"
+				<div class="col-12">
+					<h2 class="text-body-secondary">About</h2>
+					<p class="mb-1">
+						<a
+							href="https://www.nordicsemi.com/Privacy"
+							class="text-body-tertiary text-decoration-none"
 							target="_blank"
 						>
-							devzone.nordicsemi.com
-						</SecondaryLink>
+							Privacy Policy
+						</a>
+					</p>
+					<p class="mb-1">
+						<a
+							href="https://www.nordicsemi.com/About-us/Contact-Us"
+							class="text-body-tertiary text-decoration-none"
+							target="_blank"
+						>
+							Contact us
+						</a>
+					</p>
+				</div>
+			</div>
+			<div class="row mt-4">
+				<div class="col-12 d-flex flex-col justify-content-between align-items-end">
+					<div>
+						<h2 class="text-body-secondary">Support</h2>
+						<p>
+							<a href="https://devzone.nordicsemi.com/" target="_blank">
+								<img
+									src="/static/images/devzone-dark.svg"
+									alt="{DevZone"
+									width="150"
+								/>
+							</a>
+						</p>
+					</div>
+					<div>
+						<a href="https://nordicsemi.com/" target="_blank">
+							<img
+								src="/static/images/nordic-logo.svg"
+								alt="Nordic Semiconductor"
+								width="100"
+							/>
+						</a>
+					</div>
+				</div>
+			</div>
+			<div class="row mt-4">
+				<div class="col-12 text-center text-body-secondary mt-4">
+					<p>
+						© <CopyrightYear /> Nordic Semiconductor. All rights reserved.
+					</p>
+					<p class="text-body-tertiary">
+						<small>{VERSION}</small>
 					</p>
 				</div>
 			</div>
