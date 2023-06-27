8b02f8a3f34c3f9dbe966e2b7bb47fb0d7450cad
Markus Tacker <Markus.Tacker@NordicSemi.no>
2023-06-27 20:50:19 +0200

    refactor: replace with inline style

--- src/components/CountryFlag.tsx
+++ src/components/CountryFlag.tsx
@@ -1,16 +1,18 @@
-import styled from 'styled-components'
 import { mccmnc2country } from './mccmnc2country.js'
 
-export const Flag = styled.img`
-	width: 20px;
-	margin-left: 0.5rem;
-`
-
-export const CountryFlag = ({ mccmnc }: { mccmnc: number }) => {
+export const CountryFlag = ({
+	mccmnc,
+	class: c,
+}: {
+	mccmnc: number
+	class?: string
+}) => {
 	const country = mccmnc !== undefined ? mccmnc2country(mccmnc) : undefined
 	if (country === undefined) return null
 	return (
-		<Flag
+		<img
+			class={c}
+			width={20}
 			alt={country.name}
 			title={country.name}
 			src={`/static/flags/${country.code.toLowerCase()}.svg`}
