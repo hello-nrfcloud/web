6f52fc72e6041f4d3316612eba150aec6d12b16c
Markus Tacker <Markus.Tacker@NordicSemi.no>
2023-06-28 14:39:01 +0200

    fix(udpate-warning): add cache buster

--- src/components/AppUpdateNotifier.tsx
+++ src/components/AppUpdateNotifier.tsx
@@ -14,7 +14,7 @@ export const AppUpdateNotifier = () => {
 	const [ignored, setIgnored] = useState<string[]>(getStored())
 
 	const checkVersion = useCallback(() => {
-		fetch('/.well-known/release')
+		fetch(`/.well-known/release?v=${VERSION}`)
 			.then(async (res) => (await res.text()).trim())
 			.then(parse)
 			.then((releasedVersion) => {
