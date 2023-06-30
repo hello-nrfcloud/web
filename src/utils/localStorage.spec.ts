27f10350a3573d75079a48fea1d757dea8ec0788
Markus Tacker <Markus.Tacker@NordicSemi.no>
2023-06-30 12:36:23 +0200

    test: silence console

--- src/utils/localStorage.spec.ts
+++ src/utils/localStorage.spec.ts
@@ -1,6 +1,15 @@
 import { getItem, removeItem, setItem } from './localStorage.js'
 
 describe('localStorage', () => {
+	let warn: any
+	beforeAll(() => {
+		// silence console
+		// eslint-disable-next-line @typescript-eslint/no-empty-function
+		warn = jest.spyOn(console, 'warn').mockImplementation(() => {})
+	})
+	afterAll(() => {
+		warn.mockReset()
+	})
 	it('should swallow calls when run server side', () => {
 		expect(setItem('foo', 'bar')).toEqual(undefined)
 		expect(getItem('foo')).toEqual(null)
