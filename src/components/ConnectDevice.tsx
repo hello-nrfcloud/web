d4ff3a661f6d04c2c77e0deeafc47ff528676b56
Markus Tacker <Markus.Tacker@NordicSemi.no>
2023-08-10 16:58:08 +0200

    feat: add info about SIM card
    Fixes #89

--- src/components/ConnectDevice.tsx
+++ src/components/ConnectDevice.tsx
@@ -2,6 +2,7 @@ import { Ago } from '#components/Ago.js'
 import { type Device } from '#context/Device.js'
 import { WaitingForData } from '#components/WaitingForData.js'
 import { BatteryFull, CloudOff, ToggleRight } from 'lucide-preact'
+import { SIMIcon } from '#components/icons/SIMIcon.js'
 
 export const ConnectDevice = ({ device }: { device: Device }) => (
 	<>
@@ -23,6 +24,30 @@ export const ConnectDevice = ({ device }: { device: Device }) => (
 					<p>
 						<ToggleRight class="me-2" />
 						Turn the kit on
+					</p>
+					<p>
+						<SIMIcon class="me-2" /> Sufficient data left on the SIM?
+						<br />
+						<small>
+							This model comes with a <strong>pre-activated</strong>{' '}
+							<a
+								href="https://ibasis.com/solutions/esim-technology/"
+								target="_blank"
+								rel="noreferrer noopener"
+							>
+								SIM card from iBasis
+							</a>
+							. You may run of data depending on your usage. You can check the
+							the amount of data left on your SIM card{' '}
+							<a
+								href="https://developer.nordicsemi.com/nRF_Connect_SDK/doc/latest/nrf/device_guides/working_with_nrf/nrf91/thingy91_gsg.html#connecting-to-nrf-cloud"
+								target="_blank"
+								rel="noreferrer noopener"
+							>
+								by adding the SIM to your nRF Cloud account
+							</a>
+							.
+						</small>
 					</p>
 				</div>
 				<div class="col-12 col-lg-6">
