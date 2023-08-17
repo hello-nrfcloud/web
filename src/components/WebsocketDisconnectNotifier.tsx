eda3e8a7e6495eb4300b2368b7d86c13afc12434
Markus Tacker <Markus.Tacker@NordicSemi.no>
2023-08-17 14:44:52 +0200

    fix(websocket-warning): align with update warning

--- src/components/WebsocketDisconnectNotifier.tsx
+++ src/components/WebsocketDisconnectNotifier.tsx
@@ -5,13 +5,17 @@ export const WebsocketDisconnectNotifier = () => {
 	const { disconnected } = useDevice()
 	if (!disconnected) return null
 	return (
-		<div role="alert" class="bg-fall">
-			<div class="container py-2 d-flex align-items-center">
-				<ZapOff strokeWidth={1} class="me-2" />
-				<span class="me-2">
-					The websocket connection was terminated automatically after 2 hours.
+		<div role="alert" class="bg-sun">
+			<div class="container py-2  d-flex justify-content-between align-items-center">
+				<span>
+					<ZapOff strokeWidth={1} class="me-2" />
+					<span class="me-2">
+						The websocket connection was terminated automatically after 2 hours.
+					</span>
 				</span>
-				<a href="/device">Click here to reload.</a>
+				<a href="./" class="btn btn-outline-secondary ms-2">
+					reload
+				</a>
 			</div>
 		</div>
 	)
