9e043cab2bea97360ec7e7da4096bbada6aa46dd
Markus Tacker <Markus.Tacker@NordicSemi.no>
2024-02-22 16:14:47 +0100

    fix(sharing): update public link

--- src/map/publicDeviceLink.ts
+++ src/map/publicDeviceLink.ts
@@ -2,6 +2,4 @@ import type { PublicDevice } from '@hello.nrfcloud.com/proto/hello/map'
 import type { Static } from '@sinclair/typebox'
 
 export const publicDeviceURL = (device: Static<typeof PublicDevice>): URL =>
-	new URL(
-		`https://hello.nrfcloud.com/map#${encodeURIComponent(`id:${device.id}`)}`,
-	)
+	new URL(`https://hello.nrfcloud.com/map/#id:${device.id}`)
