45fe7d5915d5ae53c714a922edd8f06e9f8e50f3
Markus Tacker <Markus.Tacker@NordicSemi.no>
2024-06-10 16:55:01 +0200

    fix: update dependencies

--- src/proto/validPassthrough.spec.ts
+++ src/proto/validPassthrough.spec.ts
@@ -1,6 +1,9 @@
 import { validPassthrough } from './validPassthrough.js'
 import { describe, test as it, mock } from 'node:test'
 import assert from 'node:assert'
+import shadow from './shadow.json'
+import { validateWithTypeBox } from '@hello.nrfcloud.com/proto'
+import { Shadow } from '@hello.nrfcloud.com/proto/hello'
 
 void describe('validPassthrough', () => {
 	void it('should let valid input pass', () => {
@@ -27,5 +30,14 @@ void describe('validPassthrough', () => {
 		assert.deepEqual(call?.arguments[0], { temp: -42 })
 		// Errors
 		assert.equal(Array.isArray(call?.arguments[1]), true)
+	})
+
+	void it('should validate a shadow document', () => {
+		const v = validateWithTypeBox(Shadow)
+		const maybeValidMessage = v(shadow)
+		assert.equal('errors' in maybeValidMessage, false)
+
+		const isValid = validPassthrough(shadow, console.error)
+		assert.deepEqual(isValid, shadow)
 	})
 })
