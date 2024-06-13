6b09c412d9a305b6a0ffc37a5143cc6be6f76efd
Markus Tacker <Markus.Tacker@NordicSemi.no>
2024-06-13 14:34:40 +0200

    fix(colorpicker): ensure color values are 8 Bit integer
    See https://nordic-semiconductor-asa.sentry.io/share/issue/f7659fca34a0409eb82b1b2c25a4f03f/

--- src/components/colorpicker/isRGB.ts
+++ src/components/colorpicker/isRGB.ts
@@ -2,4 +2,13 @@ import { isObject } from 'lodash-es'
 import type { RGB } from './ColorPicker.js'
 
 export const isRGB = (color: unknown): color is RGB =>
-	isObject(color) && 'r' in color && 'b' in color && 'g' in color
+	isObject(color) &&
+	'r' in color &&
+	isEightBitInt(color.r) &&
+	'g' in color &&
+	isEightBitInt(color.g) &&
+	'b' in color &&
+	isEightBitInt(color.b)
+
+const isEightBitInt = (value: unknown): value is number =>
+	typeof value === 'number' && value >= 0 && value <= 255
