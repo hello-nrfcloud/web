ae66a9d74f73eb95f624ee2137db2c6670c20142
Markus Tacker <Markus.Tacker@NordicSemi.no>
2024-06-21 13:44:38 +0200

    fix(app-update): align layout

--- src/components/AppUpdateNotifier.tsx
+++ src/components/AppUpdateNotifier.tsx
@@ -47,31 +47,35 @@ export const AppUpdateNotifier = () => {
 		}
 	}, [checkVersion])
 
+	const ignore = () => () => {
+		if (newerVersion === undefined) return
+		setItem('app:ignored_versions', [...new Set([...ignored, newerVersion])])
+		setIgnored((i) => [...new Set([...i, newerVersion])])
+		setNewerVersion(undefined)
+		console.log(`[App]`, `ignored newer version`, newerVersion)
+	}
+
 	if (newerVersion === undefined) return null
 	return (
 		<div role="alert" class="bg-sun">
-			<div class="container p-2 d-flex justify-content-between align-items-center">
-				<span>A newer version ({newerVersion}) is available.</span>
-				<span class="flex-shrink-0">
-					<button
-						type="button"
-						class="btn btn-outline-danger"
-						onClick={() => {
-							if (newerVersion === undefined) return
-							setItem('app:ignored_versions', [
-								...new Set([...ignored, newerVersion]),
-							])
-							setIgnored((i) => [...new Set([...i, newerVersion])])
-							setNewerVersion(undefined)
-							console.log(`[App]`, `ignored newer version`, newerVersion)
-						}}
-					>
-						ignore
-					</button>
-					<a href="./" class="btn btn-outline-secondary ms-2">
-						reload
-					</a>
-				</span>
+			<div class="container">
+				<div class="row">
+					<div class="col p-2 d-flex justify-content-between align-items-center">
+						<span>A newer version ({newerVersion}) is available.</span>
+						<span class="flex-shrink-0">
+							<button
+								type="button"
+								class="btn btn-outline-danger"
+								onClick={ignore}
+							>
+								ignore
+							</button>
+							<a href="./" class="btn btn-outline-secondary ms-2">
+								reload
+							</a>
+						</span>
+					</div>
+				</div>
 			</div>
 		</div>
 	)
