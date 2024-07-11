414bd6f040e696d2250297dc2fecc880b8340b31
Markus Tacker <m@coderbyheart.com>
2024-07-11 12:29:46 +0200

    feat(problem): include device ID

--- src/components/Problem.tsx
+++ src/components/Problem.tsx
@@ -1,3 +1,4 @@
+import { useDevice } from '#context/Device.js'
 import type { FetchProblem } from '#utils/validatingFetch.js'
 
 export const Problem = ({
@@ -6,51 +7,61 @@ export const Problem = ({
 }: {
 	problem: FetchProblem
 	class?: string
-}) => (
-	<div class={`alert alert-danger problem ${className}`}>
-		<p class="mb-2">
-			<strong>{problem.title}</strong>
-			{problem.detail !== undefined && (
-				<>
-					<br />
-					<small>{problem.detail}</small>
-				</>
-			)}
-		</p>
-		<p class="mb-2">
-			If this problem persists, you can{' '}
-			<a
-				href="https://devzone.nordicsemi.com/f/nordic-q-a/tags/hello-nrfcloud"
-				target="_blank"
-			>
-				search {'{'}DevZone for known issues
-			</a>
-			.
-		</p>
-		<p class="mb-2">
-			When{' '}
-			<a href="https://devzone.nordicsemi.com/support/add" target="_blank">
-				creating a new {'{'}DevZone ticket
-			</a>
-			, please include this information:
-		</p>
-		<p>
-			<small class="me-1">Browser:</small>
-			<small>{navigator.userAgent}</small>
-			{awsApiGwReqId !== undefined && (
-				<>
-					<br />
-					<small class="me-1">API Gateway Request ID:</small>
-					<code>{awsApiGwReqId}</code>
-				</>
-			)}
-			{awsReqId !== undefined && (
-				<>
-					<br />
-					<small class="me-1">Request ID:</small>
-					<code>{awsReqId}</code>
-				</>
-			)}
-		</p>
-	</div>
-)
+}) => {
+	const { device } = useDevice()
+	return (
+		<div class={`alert alert-danger problem ${className}`}>
+			<p class="mb-2">
+				<strong>{problem.title}</strong>
+				{problem.detail !== undefined && (
+					<>
+						<br />
+						<small>{problem.detail}</small>
+					</>
+				)}
+			</p>
+			<p class="mb-2">
+				If this problem persists, you can{' '}
+				<a
+					href="https://devzone.nordicsemi.com/f/nordic-q-a/tags/hello-nrfcloud"
+					target="_blank"
+				>
+					search {'{'}DevZone for known issues
+				</a>
+				.
+			</p>
+			<p class="mb-2">
+				When{' '}
+				<a href="https://devzone.nordicsemi.com/support/add" target="_blank">
+					creating a new {'{'}DevZone ticket
+				</a>
+				, please include this information:
+			</p>
+			<p>
+				<small class="me-1">Browser:</small>
+				<small>{navigator.userAgent}</small>
+				{awsApiGwReqId !== undefined && (
+					<>
+						<br />
+						<small class="me-1">API Gateway Request ID:</small>
+						<code>{awsApiGwReqId}</code>
+					</>
+				)}
+				{awsReqId !== undefined && (
+					<>
+						<br />
+						<small class="me-1">Request ID:</small>
+						<code>{awsReqId}</code>
+					</>
+				)}
+				{device !== undefined && (
+					<>
+						<br />
+						<small class="me-1">Device ID:</small>
+						<code>{device.id}</code>
+					</>
+				)}
+			</p>
+		</div>
+	)
+}
