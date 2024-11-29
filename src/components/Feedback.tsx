eb67581d283791089d27a73a71b252d0f1ee82e9
Markus Tacker <Markus.Tacker@NordicSemi.no>
2024-11-29 17:38:16 +0100

    fix(feedback): center form

--- src/components/Feedback.tsx
+++ src/components/Feedback.tsx
@@ -4,7 +4,7 @@ export const Feedback = () => (
 	<section class="pt-4 pb-4 bg-dark">
 		<div class="container mt-4">
 			<div class="row">
-				<div class="col-12 col-md-6">
+				<div class="col-12 col-md-6 offset-md-3">
 					<FeedbackForm />
 				</div>
 			</div>
