8d9a28acbd25e953c620f8c7b9a72697628c0ec3
Markus Tacker <Markus.Tacker@NordicSemi.no>
2025-01-21 13:30:32 +0100

    test(e2e): fix execution

--- e2e-tests/vite.config.e2e.ts
+++ e2e-tests/vite.config.e2e.ts
@@ -5,22 +5,24 @@ import { createContext } from './lib/mock-backend/context.js'
 import { mockWebsocket } from './lib/mock-backend/mockWebsocket.js'
 import { testdataServerPlugin } from './lib/testDataServerPlugin.js'
 
-const { mapRegion, mapName, mapApiKey } = fromEnv({
+const { mapRegion, mapName, mapApiKey, registryEndpoint } = fromEnv({
 	mapRegion: 'MAP_REGION',
 	mapName: 'MAP_NAME',
 	mapApiKey: 'MAP_API_KEY',
+	registryEndpoint: 'REGISTRY_ENDPOINT',
 })(process.env)
 
 const context = createContext(join(process.cwd(), 'db'))
 
-const base = 'http://localhost:8080'
+const domainName = 'localhost:8080'
+const base = `http://${domainName}`
 
 const wsPort = 1024 + Math.floor(Math.random() * (65535 - 1024))
 mockWebsocket(wsPort, context)
 
 export default createConfig({
-	domainName: 'localhost:8080',
-	registryEndpoint: new URL('/e2e/registry.json', base),
+	domainName,
+	registryEndpoint: new URL(registryEndpoint),
 	plugins: [
 		testdataServerPlugin({
 			registry: {
